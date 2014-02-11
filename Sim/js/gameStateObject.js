85ee72c9381210faf36cc42f8dffae23e1ab9f99
Mike Gallagher <mikesligo@gmail.com>
2014-02-11 20:44:12 +0000

    module and subsystem objects done

--- Sim/js/gameStateObject.js
+++ Sim/js/gameStateObject.js
@@ -10,8 +10,9 @@
   subsystems - Details the subsystems involved in the manager's project
  */
  
-function game_state()
+function GameState()
 {
+
 	this.sites = [];
 	this.current_time = "";
 	this.tasks = [];
@@ -19,29 +20,47 @@ function game_state()
 	this.development_type = "";
 	this.problems = [];
 	this.finance = [];
-	this.modules = [];
-	this.subsystems = [];	
+
+    main_module = new Module("write backend", [new Subsystem("write model",30), new Subsystem("write view", 25), new Subsystem("write controller", 35)]);
+	this.modules = [main_module];
 }
-game_state.prototype.change_sites = function(val){this.sites = val;}
-game_state.prototype.change_time = function(val){this.current_time = val;}
-game_state.prototype.change_tasks = function(val){this.tasks = val;}
-game_state.prototype.change_real_task_effort = function(val){this.real_task_effort = val;}
-game_state.prototype.change_development_type = function(val){this.development_type = val;}
-game_state.prototype.change_problems = function(val){this.problems = val;}
-game_state.prototype.change_finance = function(val){this.finance = val;}
-game_state.prototype.change_modules = function(val){this.modules = val;}
-game_state.prototype.change_subsystems = function(val){this.subsystems = val;}
+
+GameState.prototype.add_sites = function(site){this.sites.push(site);}
+GameState.prototype.change_time = function(val){this.current_time = val;}
+GameState.prototype.change_tasks = function(val){this.tasks = val;}
+GameState.prototype.change_real_task_effort = function(val){this.real_task_effort = val;}
+GameState.prototype.change_development_type = function(val){this.development_type = val;}
+GameState.prototype.change_problems = function(val){this.problems = val;}
+GameState.prototype.change_finance = function(val){this.finance = val;}
+GameState.prototype.add_modules = function(module){this.modules.push(module);}
+
+function Site(name, coordinates, culture_modifier){
+    this.name = name;
+    this.coordinates = coordinates;
+    this.culture = culture_modifier; //obj
+}
+
+function Module(name, subsystems){
+    this.name = name;
+    this.subsystems = subsystems; //obj list
+}
+
+function Subsystem(name, total){
+    this.name = name;
+    this.completed = 0;
+    this.total = total;
+}
 
 //A quick function to initialise the game state with some ints and strings and print them
 //to the console
-function init_game_state()
+function init_GameState()
 {
-	var gs = new game_state();
+	var gs = new GameState();
 	iterate(gs);
 	return gs;
 }
 
-function game_state_to_json(obj)
+function GameState_to_json(obj)
 {
 	var result = JSON.stringify(obj);
 	return result;
@@ -59,9 +78,9 @@ function iterate(obj)
 }
 
 // Barebones game state update loop
-function simpleTick(game_state)
+function simpleTick(GameState)
 {
-	var gs = game_state;
+	var gs = GameState;
 	// Increment current time
 	// Todo: Decide how to represent time
 	gs.current_time++;
@@ -76,18 +95,31 @@ function simpleTick(game_state)
 
 // Example 'module.update()' function
 // Having each module implement its own update() allows for modular behaviour
-function update(game_state)
+function update(GameState)
 {
 	// Do different stuff depening on what development method
 	// E.G increment current task progress, check if we can move to next task etc
-	switch (game_state.development_type) {
+	switch (GameState.development_type) {
 		case "Waterfall":
 			console.log("Updating in a waterfall fashion!");
 			break;
 		case "Agile":
 			console.log("Look at me, aren't I agile?");
 			break;
+		case "Reddit":
+			console.log("Will update gamestate after reddit");
+			break;
 		default:
 			console.log("What even IS this development methodology?");
 	}
+}
+
+if (require.main === module){ 
+    console.log("Node method for debugging");
+    var gs = new GameState();
+    console.log(gs);
+    for (var i=0;i < gs.modules.length; i++){
+        console.log(gs.modules[i]);
+    }
+    console.log (GameState_to_json(gs));
 }
