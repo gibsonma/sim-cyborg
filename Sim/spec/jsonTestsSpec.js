25ed9e3b47dcce2686fb57431fab32c9865ba6ee
Michael Gallagher <mikesligo@gmail.com>
2014-02-12 20:41:47 +0000

    making test look on relative path instead of specific hostname

--- Sim/spec/jsonTestsSpec.js
+++ Sim/spec/jsonTestsSpec.js
@@ -1,6 +1,6 @@
 describe("Json Related Functions", function() {
 
     it("checks if file is at specific url", function() {
-        expect(jsonExistsParses('http://localhost:3000/data/config_file.json')).toEqual(true);
+        expect(jsonExistsParses('data/config_file.json')).toEqual(true);
     });
 });
