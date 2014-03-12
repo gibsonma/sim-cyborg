95081d7ca53a070b411d599b7979c5a87a15a5ab
Mike Gallagher <mikesligo@gmail.com>
2014-03-12 12:46:31 +0000

    merge conflicts

--- Sim/src/state.js
+++ Sim/src/state.js
@@ -155,6 +155,7 @@ function Site(name, coordinates, culture_modifier, dev, timezone, home){
     this.timezone = timezone;
     this.problemCooldown = 0.0025;
     this.home = home;
+    this.local_time = 0;
 }
 
 function Culture(influence){
